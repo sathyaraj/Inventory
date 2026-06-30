ce6c30c0b4ec4e0216fdf208aab5f0682bbb97c6
unknown <sathyaraj.air@gmail.com>
2026-06-30 15:43:06 +0530

    updated production api url to render api name

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   //apiUrl: 'https://localhost:7108/api'
 
-  apiUrl: 'https://inventory-api-7-vkqm.onrender.com'
+  apiUrl: 'https://inventory-api-7-vkqm.onrender.com/api'
 };
